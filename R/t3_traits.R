7143efc9a4eb1dffa76584df43305985d8545fdf
neyhartj <neyhartje@gmail.com>
2018-03-27 10:00:48 -0500

    Edits to the project file and added T3 traits

---
+++ R/t3_traits.R
@@ -0,0 +1,20 @@
+#' T3 Trait Descriptions
+#' 
+#' @description 
+#' A \code{data.frame} of trait names and descriptions provided by the T3
+#' database.
+#' 
+#' @format A data frame with 138 rows and 8 columns:
+#' \describe{
+#'    \item{Trait}{The name used by T3 for the trait}
+#'    \item{Nickname}{A nickname for the trait}
+#'    \item{Ontology}{}
+#'    \item{Description}{}
+#'    \item{Min}{Minimum value of the trait}
+#'    \item{Max}{Maximum value of the trait}
+#'    \item{Unit}{Unit of the trait}
+#'    \item{Unit.info}{}
+#' }
+#' 
+#' @source \url{https://t3sandbox.org/t3/sandbox/barley/traits.php}
+"t3_traits"
